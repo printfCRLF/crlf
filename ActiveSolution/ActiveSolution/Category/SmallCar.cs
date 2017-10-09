454aaf2a4227af4200f6f56e3a5b63d38fff5570
bowen <bowen@BOWEN-DELLE6420>
2017-10-09 21:02:03 +0200

    ICarCategory and three classes

---
+++ ActiveSolution/ActiveSolution/Category/SmallCar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveSolution.Category
+{
+    class SmallCar : ICarCategory
+    {
+        public decimal DayPriceFactor { get { return 1.0M; } }
+        
+
+        public decimal DistancePriceFactor { get { return 0.0M; } }
+
+        public decimal CalculatePrice(decimal pricePerDay, int days, decimal pricePerKilometer, decimal kilometers)
+        {
+            return pricePerDay * days * DayPriceFactor + pricePerKilometer * kilometers * DistancePriceFactor;
+        }
+    }
+}
---
+++ ActiveSolution/ActiveSolution/Category/Truck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveSolution.Category
+{
+    class Truck : ICarCategory
+    {
+        public decimal DayPriceFactor { get { return 1.5M; } }
+
+
+        public decimal DistancePriceFactor { get { return 1.5M; } }
+
+        public decimal CalculatePrice(decimal pricePerDay, int days, decimal pricePerKilometer, decimal kilometers)
+        {
+            return pricePerDay * days * DayPriceFactor + pricePerKilometer * kilometers * DistancePriceFactor;
+        }
+    }
+}
