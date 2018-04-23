ff1121bbaaaa2e3cb876005cf4d84a14233ccfa3
Bowen <bowen.sui@hotmail.com>
2018-04-23 14:44:46 +0200

    Recreate solutions. Samurai.Data and Samurai.Domain are .NET Standard projects. They install EntityFrameworkCore nuget packages. WebAppCore is an asp.net core project. It references Data and Domain. WebAppCore injects DbContextOptions<SamuraiContext>

---
+++ JulieLerman.EfCore2/SamuraiApp.Data.Standard/SamuraiContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain.Standard;
+
+namespace SamuraiApp.Data.Standard
+{
+    public class SamuraiContext : DbContext
+    {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    var cs = "Data Source=localhost;Initial Catalog=SamuraiAppDotNet;Integrated Security=True";
+        //    optionsBuilder.UseSqlServer(cs);
+        //}
+    }
+}
