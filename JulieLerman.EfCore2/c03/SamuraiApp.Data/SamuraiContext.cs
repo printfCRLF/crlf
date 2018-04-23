8d1575ff3d62d3358c1bcbccf8835e8f5b11a6a2
Bowen <bowen.sui@hotmail.com>
2018-04-23 16:43:12 +0200

    Insert Data

--- JulieLerman.EfCore2/c03/SamuraiApp.Data/SamuraiContext.cs
+++ JulieLerman.EfCore2/c03/SamuraiApp.Data/SamuraiContext.cs
@@ -1,18 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
-    public class SamuraiContext:DbContext
+    public class SamuraiContext : DbContext
     {
+        public static readonly LoggerFactory MyConsoleLoggerFactory =
+            new LoggerFactory(new[]
+            {
+                new ConsoleLoggerProvider((category, level) =>
+                    category == DbLoggerCategory.Database.Command.Name
+                    && level == LogLevel.Information, true)
+            });
+
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
-                 "Server = (localdb)\\mssqllocaldb; Database = SamuraiAppData; Trusted_Connection = True; ");
+            optionsBuilder
+                .UseLoggerFactory(MyConsoleLoggerFactory)
+                .EnableSensitiveDataLogging(true)
+                .UseSqlServer(
+                    "Data Source=localhost;Initial Catalog=SamuraiAppCore;Integrated Security=True");
+            //"Server = (localdb)\\mssqllocaldb; Database = SamuraiAppData; Trusted_Connection = True; ");
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
